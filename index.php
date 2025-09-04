dfc00e765825e3a7042d34b3871da682af33584c
Maribel Arikainen <maribel.arikainen@tptlive.ee>
2025-09-04 10:12:48 +0300

    Add variable examples

---
+++ index.php
@@ -0,0 +1,25 @@
+<?php
+
+echo "Hello World!\n";
+echo "Hello World!\n";
+$name = 'Maribel';
+echo 'Hello ' . $name;
+echo "\n";
+$age = 19;
+echo 'Your name is ' . $name . ' and you are ' . $age . ' years old!';
+echo "\n";
+echo "Your name is $name and you are $age years old!\n";
+
+$var = 1;
+var_dump($var);
+$var = 1.3;
+var_dump($var);
+$var = 'Hellõ';
+var_dump($var);
+$var = true;
+var_dump($var);
+$var = false;
+var_dump($var);
+$bar = null;
+var_dump($bar);
+?>
