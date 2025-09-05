0110a8975f398b03b8046cecfb98d4c894c0681d
Maribel Arikainen <maribel.arikainen@tptlive.ee>
2025-09-05 14:01:33 +0300

    Add construct

--- index.php
+++ index.php
@@ -5,25 +5,26 @@ class Box {
     public $width;
     public $length;
 
+    public function __construct($height=0, $width=0, $length=0){
+        $this->height = $height;
+        $this->width = $width;
+        $this->length = $length;
+    }
+
     public function volume(){
         return $this->height * $this->width * $this->length;
     }
 }
 
-$box1 = new Box();
-$box1->height = 10;
-$box1->width = 10;
-$box1->length = 10;
+$box1 = new Box(10, 10, 10);
 $volume = $box1->volume();
 var_dump($box1);
 var_dump($volume);
 var_dump($box1->volume());
 
 
-$box2 = new Box();
-$box2->height = 40;
-$box2->width = 60;
-$box2->length = 70;
+$box2 = new Box(30, 40, 50);
+
 var_dump($box2);
 var_dump($box2->volume());
 var_dump($box1);
