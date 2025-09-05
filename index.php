dd33a05a809225826bbb83d749cb8b800359762d
Maribel Arikainen <maribel.arikainen@tptlive.ee>
2025-09-05 13:29:39 +0300

    Add function examples

--- index.php
+++ index.php
@@ -1,44 +1,33 @@
 <?php
-var_dump(PHP_INT_MAX);
-for($i=0; $i<10; $i++){
-    var_dump($i);
+
+function sayHello(){
+    var_dump('Hello');
 }
 
-for($i=10; $i>0; $i--){
-    var_dump($i);
+sayHello();
+
+function sayHelloToMe($name='Nameless'){
+    var_dump("Hello $name!");
 }
 
-for($i=1; $i<1_000_000; $i*=2){
-    var_dump($i);
-}
-// $current = (int) date("s");
+sayHelloToMe('Maribel');
+sayHelloToMe('Olga');
+sayHelloToMe();
 
-// while($current<59){
-//     var_dump($current);
-//     $current = (int) date("s");
-// }
-$i = 10;
-
-while($i<10){
-    var_dump('While');
+function sayNameAndAge($name, $age) {
+    var_dump("Your name is $name and yoy are $age years old!");
 }
 
-do {
-    var_dump('Do While');
-} while($i<10);
+sayNameAndAge('Maribel', 19);
+sayNameAndAge('Olga', 18);
 
-$array = ['Apples', 'Oranges', 'Pears'];
 
-foreach($array as $value) {
-    var_dump($value);
+function recursion($i){
+    if($i<10){
+        var_dump($i);
+        recursion($i+1);
+    }
 }
 
-$array = [
-    'name' => 'Maribel',
-    'age' => 19,
-    'isFemale' => true,
-];
 
-foreach($array as $key => $value) {
-    var_dump($key, $value);
-}
+recursion(0);
